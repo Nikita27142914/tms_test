7d77cf09e4d0bcd319557398e0da622b81730bb6
Mikita.Nikolski <Mikita.Nikolski@Connectwise.com>
2021-11-26 19:48:01 +0300

    add: second task

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var a = 2;
+var b = 'str';
+var c = true;
+
+var typeA = typeof(a); // "number"
+var typeB = typeof(b); // "string"
+var typeC = typeof(c); // "boolean"
+
+console.log(`Тип переменной a со значением ${a}: ${typeA}`);
+console.log(typeof(typeA));
+
+console.log(`Тип переменной b со значением ${b}: ${typeB}`);
+console.log(typeof(typeB));
+
+console.log(`Тип переменной c со значением ${c}: ${typeC}`);
+console.log(typeof(typeC));
+
+
